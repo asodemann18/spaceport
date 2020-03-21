1a9d1eec753dd266e6a2e4381e78c75a8a5df4be
Alex Sodemann <asodemann18@gmail.com>
2020-03-20 20:57:50 -0600

    Complete tests for being.js

--- src/being.js
+++ src/being.js
@@ -1,4 +1,16 @@
 class Being {
+  constructor(name,species){
+    this.isAlive = true;
+    this.name = name;
+    this.species = species;
+    this.credits = 0;
+  }
+
+  updateCredits(newCredits){
+
+  return this.credits = newCredits + this.credits;
+
+  }
 }
 
 module.exports = Being;

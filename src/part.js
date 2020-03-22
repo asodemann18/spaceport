c83ec9c4fa8b15bda580d980b009e84641d7dd17
Alex Sodemann <asodemann18@gmail.com>
2020-03-22 16:36:45 -0600

    Complete tests for part.js

---
+++ src/part.js
@@ -0,0 +1,22 @@
+class Part {
+  constructor({name, type, value}) {
+  this.name = name;
+  this.validTypes = [
+    'shell',
+    'hyperdrive',
+    'computer',
+    'life support',
+    'landing gear',
+    undefined
+  ];
+  this.type = this.validTypes.includes(type) ? type : undefined;
+  this.value = value;
+  this.broken = false;
+}
+
+  isValid() {
+    return !!(this.name && this.type && this.value);
+  }
+}
+
+module.exports = Part;
